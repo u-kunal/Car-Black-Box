29f7eb7bc28e654291d280bf07e98fae2d874080
Kunal U <kunalu495@gmail.com>
2025-02-12 20:28:53 +0530

    Add files via upload

---
+++ timers.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   timers.h
+ * Author: ACER
+ *
+ * Created on 28 Jan, 2025, 6:42 PM
+ */
+
+#ifndef TIMERS_H
+#define	TIMERS_H
+
+void init_timer0(void);
+void init_timer2(void);
+
+#endif	/* TIMERS_H */
+
